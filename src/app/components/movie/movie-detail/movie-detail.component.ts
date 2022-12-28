893750335154ffc4add193c21a6e6e69be9753c1
Patryk <patrykyrtap@o2.pl>
2022-12-28 20:25:11 +0100

    Implement movie detail, footer

--- src/app/components/movie/movie-detail/movie-detail.component.ts
+++ src/app/components/movie/movie-detail/movie-detail.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieService} from "../../../services/movie.service";
+import {Movie} from "../../../interfaces/movie";
+import {ActivatedRoute} from "@angular/router";
+import * as urlPaths from '../../../constants/urlPaths';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss']
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
+
+  movie: Movie | undefined
+  urlPaths = urlPaths;
+
+  constructor(private route: ActivatedRoute, private movieService: MovieService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.movieService.getMovieById(params['id']).subscribe(data => {
+       this.movie = data;
+      });
+    });
+  }
+
+  setBackground() {
+    if (!this.movie || !this.movie.backdrop_path) {
+      return {
+        'background-color': 'black',
+        'width': '100px',
+        'height': '100px',
+      };
+    }
+    return {
+      'background-image': urlPaths.coverPath(this.movie.backdrop_path),
+      'width': '100px',
+      'height': '100px',
+    };
+  }
 
 }
--- src/app/constants/urlPaths.ts
+++ src/app/constants/urlPaths.ts
@@ -2,7 +2,7 @@ import { environment } from "../../environments/environment";
 
 const BASE_URL = environment.apiURL;
 const API_KEY = environment.apiKey;
-export const getMovieById = (id: number) => `${BASE_URL}/movie${id}?api_key=${API_KEY}`;
+export const getMovieById = (id: number) => `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`;
 export const getPopularMovies = (page: number = 1) => `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`;
 export const latestMovie = () => `${BASE_URL}/movie/latest?api_key=${API_KEY}&language=en-US`;
 export const searchMovie = (phrase: string) => `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${phrase}`;
